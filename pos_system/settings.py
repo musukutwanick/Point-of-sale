2fb5dca2e37c8554e6c7074df4c2cd0d5f987217
Nick Musukutwa <musukutwan@Cell.co.zw>
2026-04-05 20:47:35 +0200

    date

--- pos_system/settings.py
+++ pos_system/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'pos_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('SQLITE_PATH', str(BASE_DIR / 'db.sqlite3')),
     }
 }
 
